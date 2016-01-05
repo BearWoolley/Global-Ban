ec6e11ce74e8c9366e0a9774569c0c49c088e20e
Jwoolley <jwoolley44@hotmail.co.uk>
2016-01-05 15:06:50 +0000

    Created language files

---
+++ lang/lang_french.php
@@ -0,0 +1,188 @@
+<?php
+
+$lang = array(
+
+	//Nav Bar
+	"home" => "Home"
+	"allbans" => "All Bans",
+	"activebans" => "Active Bans",
+	"history" => "History",
+	"search" => "Seach",
+	"loggedin" => "Logged in as:",
+	"admin" => "Admin",
+	"help" => "Wiki/Help",
+	"logout" => "Logout",
+
+	//Footer
+	"copyright" => "Copyright",
+	"created" => "Created By",
+
+	//Common
+	"game" => "Game",
+	"date" => "Date/Time",
+	"name" => "Name",
+	"state" => "State",
+	"steamid" => "Steam ID",
+	"view" => "View",
+	"reason" => "Reason",
+	"delete" => "Delete",
+	"communityid" => "Community ID",
+	"email" => "Email",
+	"type" => "Type",
+	"enabled" => "Enabled",
+	"disabled" => "Disabled",
+	"number" => "#",
+	"accept" => "Accept",
+	"decline" => "Decline",
+
+	//Messages
+	"youmustbeloggedin" => "You must be logged in to appeal a ban",
+	"nopermission" => "You do not have permission to appeal this ban",
+	"appealmadebold" => "The appeal has been successfully made!",
+	"appealmade" => "Please wait for an admin to review the appeal.",
+	"appealacceptedbold" => "The appeal has been accepted!",
+	"appealaccepted" => "The player has now been unbanned.",
+	"appealdeclinedbold" => "The appeal has been declined!",
+	"appealdeclined" => "The player will remain banned.",
+	"serverdelete" => "Server has been successfully deleted!",
+	"serveradded" => "Server has been successfully been added!",
+	"installcompletebold" => "The installtion is now complete!",
+	"installcompelte" => "You are now free to use the application.",
+
+	//Home
+
+	//Servers
+	"os" => "OS",
+	"vac" => "VAC",
+	"hostname" => "Hostname",
+	"players" => "Players"
+	"map" => "Map",
+	"join" => "Join",
+	"noservers" => "No servers exist",
+
+	//Perma Bans
+	"permabans" => "Latest permanent bans",
+	"totalpermabans" => "Total permanent bans:",
+	"nopermabans" => "No current permanent bans",
+
+	//Temp Bans
+	"tempbans" => "Latest temp bans",
+	"totaltempbans" => "Total temp bans:",
+	"unbanned" => "Unbanned at",
+	"notempbans" => "No current temp bans",
+
+	//Ban Details
+	"bandetails" => "Ban Details",
+	"namebanned" => "Name when banned:",
+	"currentname" => "Current name:",
+	"location" => "Location:",
+	"profile" => "Profile:",
+	"timeofban" => "Time of ban:",
+	"server" => "Server:",
+	"bannedby" => "Banned by:",
+	"unbantime" => "Unban time:",
+	"unbannedby" => "Unbanned by:",
+	"historyreason" => "User has had %s previous ban(s)",
+	"appealthisban" => "Appeal this ban",
+	"nobanselected" => "No ban selected. Please go back and select a ban.",
+
+	"appealhistory" => "Appeal History",
+	"lastupdatedate" => "Last update date",
+	"lastupdatedby" => "Last update by",
+
+	//Ban Appeal
+	"banappeal" => "Ban Appeal Form",
+	"unbanreason" => "Why should you be unbanned?:",
+	"banappealbtn" => "Submit Ban Appeal",
+
+	//All Bans
+	"noallbans" => "No bans exist",
+
+	//Active bans
+	"noactivebans" => "No active bans",
+
+	//History
+	"nohistorybans" => "No history of bans",
+
+	//Search
+	"searchuser" => "Search for a user",
+	"entersteamid" => "Enter Steam ID...",
+	"search" => "Search",
+
+	//Admin Pages
+	"adminhome" => "Admin Home",
+	"appeals" => "Appeals",
+	"banplayer" => "Ban a player",
+	"blockplayer" => "Block a player",
+	"manageservers" => "Manage Servers",
+	"manageadmins" => "Manage Admins",
+	"settings" => "Settings",
+	"wiki" => "Wiki",
+
+	//Admin Home
+	"welcome" => "Welcome,",
+	"statistics" => "Statistics",
+	"pendingappeals" => "Pending Appeals:",
+	"acceptedappeals" => "Accepted Appeals:",
+	"declinedappeals" => "Declined Appeals:",
+	"adminpermabans" => "Permament Bans:",
+	"admintempbans" => "Temp Bans:",
+	"admintotalbans" => "Total Bans:",
+
+	//Appeals
+	"pendingbanappeals" => "Pending Ban Appeals",
+	"status" => "Status",
+	"datedecision" => "Date/Time of decision",
+
+	"historyofappeals" => "History of ban appeals",
+	"dateofappeal" = "Date/Time of appeal",
+
+	"banappealmessage" => "ban appeal",
+	"banid" => "Ban ID",
+	"vacbans" => "Number of VAC bans",
+
+	//Ban a player
+	"banlength" => "Ban Length (0 for permanent):",
+	"banplayerbtn" => "Ban Player",
+
+	//Block a user
+	"blockmessage" => "Prevent a user from being able to make ban appeals.",
+	"blockuserbtn" => "Block User",
+	"noblockusers" => "No Blocked Users",
+
+	//Manage Servers
+	"serverid" => "Server ID",
+	"host" => "Host",
+	"addserver" => "Add a new server",
+	"exampleserverid" => "Example: ttt, darkrp, deathrun, bhop, etc",
+	"examplehost" => "Example: 127.0.0.1:27015",
+	"addserverbtn" => "Add Server",
+
+	//Manage admins
+	"adminsuperadmin" => "Admin/Super Admin",
+	"addnewadmin" => "Add a new admin",
+	"emailoptional" => "Email (Optional)",
+
+	//Settings
+	"generalsettings" => "General Settings",
+	"websitename" => "Website name:",
+	"websitelink" => "Website link:",
+	"changelogo" => "Change logo:",
+	"navbackgroundcolor" => "Navigation bar background colour:",
+	"navtextcolour" => "Navigation bar text colour:"
+	"updateinformation" => "Update information",
+
+	"emailsettings" => "Email Settings",
+	"enableemails" => "Enable email support:"
+	"emailtype" => "Email type:"
+	"smtphostname" => "SMTP Hostname:"
+	"smtpexample" => "E.g. smtp@example.com",
+	"port" => "Port:",
+	"portexample" => "E.g. 465",
+	"username" => "Username:",
+	"password" => "Password:",
+	"smtptype" => "SMTP type",
+	"emailaddrees" => "Email addrees:",
+	"emailfrom" => "Email from:",
+	);
+?>
