a8bc4f8fcd3899b2eb4db2923eb4de839c840834
Jwoolley <jwoolley44@hotmail.co.uk>
2016-01-06 18:37:21 +0000

    Translations for Dutch

--- lang/lang_nl.php
+++ lang/lang_nl.php
@@ -1,233 +1,234 @@
 <?php
-
+ 
 //Dutch translations for Global Ban
-
+//Translations by Infected: http://steamcommunity.com/id/anonymouseagle/
+ 
 $lang = array(
-
-	//Nav Bar
-	"bans" => "Bans",
-	"home" => "Home",
-	"allbans" => "All Bans",
-	"activebans" => "Active Bans",
-	"history" => "History",
-	"search" => "Search",
-	"loggedin" => "Logged in as:",
-	"admin" => "Admin",
-	"help" => "Wiki/Help",
-	"logout" => "Logout",
-
-	//Footer
-	"copyright" => "Copyright",
-	"created" => "Created By",
-
-	//Common
-	"game" => "Game",
-	"date" => "Date/Time",
-	"name" => "Name",
-	"state" => "State",
-	"steamid" => "Steam ID",
-	"view" => "View",
-	"reason" => "Reason",
-	"delete" => "Delete",
-	"communityid" => "Community ID",
-	"email" => "Email",
-	"type" => "Type",
-	"enabled" => "Enabled",
-	"disabled" => "Disabled",
-	"number" => "#",
-	"accept" => "Accept",
-	"decline" => "Decline",
-	"status" => "Status",
-
-	//Error messages
-	"installerror" => "<b>Error whilst loading Application. Please check your config and make sure you have ran the install: www.{your-website}.com/bans/admin/install.php</b>",
-	"signin" => "<b>Please sign in to see this page</b>",
-	"nopermissionerror" => "<b>You do not have permission to be here</b>",
-	"setupcomplete" => "<b>Error whilst loading Application. The setup has already been performed!</b>",
-
-	//Messages
-	"youmustbeloggedin" => "You must be logged in to appeal a ban",
-	"nopermission" => "You do not have permission to appeal this ban",
-	"appealmadebold" => "The appeal has been successfully made!",
-	"appealmade" => "Please wait for an admin to review the appeal.",
-	"appealopen" => "You already have a appeal open please wait for a admin to review it!",
-	"appealblockedbold" => "It seems you have been banned from making appeals",
-	"appealblocked" => "If you believe this is a mistake please contact a admin",
-	"appealacceptedbold" => "The appeal has been accepted!",
-	"appealaccepted" => "The player has now been unbanned.",
-	"appealdeclinedbold" => "The appeal has been declined!",
-	"appealdeclined" => "The player will remain banned.",
-	"serverdeleted" => "Server has been successfully deleted!",
-	"serveradded" => "Server has been successfully added!",
-	"installcompletebold" => "The installation is now complete!",
-	"installcompelte" => "You are now free to use the application.",
-	"playerbanned" => "Player has been banned",
-	"playerblocked" => "Player has been blocked from making appeals",
-	"playerunblocked" => "Player has been removed from the blocked list",
-	"admindeleted" => "Admin has been successfully deleted",
-	"adminadded" => "Admin has been successfully added",
-	"settingsupdatedbold" => "The settings have been updated!",
-	"settingsupdated" => "To pick up the new changes you need to go to the home page or click settings again",
-	"uploadfailedbold" => "The uploaded image did not have the right file type!",
-	"uploadfailed" => "Only the following types are allowed: .gif .jpg .png",
-
-	//Install
-	"welcominstall" => "Welcome to Global Ban setup. Please follow all instructions.",
-	"databasecreated" => "The following database has been created <b>%s</b> and all required tables have been made in your database.",
-	"enterdetails" => "Please enter all the details in for the default admin.",
-	"installcommunityid" => "Steam Community ID: (E.g. 76561198030856249)",
-	"installemailaddress" => "Email Address: (Optional. Used to send emails when a new appeal has been submitted)",
-	"submitadmin" => "Submit Admin",
-
-	//Home
-
-	//Servers
-	"os" => "OS",
-	"vac" => "VAC",
-	"hostname" => "Hostname",
-	"players" => "Players",
-	"map" => "Map",
-	"join" => "Join",
-	"noservers" => "No servers exist",
-	"failedquery" => "Failed to query the server. Please check the server!",
-
-	//Perma Bans
-	"permabans" => "Latest permanent bans",
-	"totalpermabans" => "Total permanent bans:",
-	"nopermabans" => "No current permanent bans",
-
-	//Temp Bans
-	"tempbans" => "Latest temp bans",
-	"totaltempbans" => "Total temp bans:",
-	"unbanned" => "Unbanned at",
-	"notempbans" => "No current temp bans",
-
-	//Ban Details
-	"bandetails" => "Ban Details",
-	"namebanned" => "Name when banned:",
-	"currentname" => "Current name:",
-	"location" => "Location:",
-	"profile" => "Profile:",
-	"timeofban" => "Time of ban:",
-	"server" => "Server:",
-	"bannedby" => "Banned by:",
-	"unbantime" => "Unban time:",
-	"unbannedby" => "Unbanned by:",
-	"historyreasonnobans" => "User has no previous bans",
-	"historyreason" => "User has had %s previous ban(s)",
-	"appealthisban" => "Appeal this ban",
-	"appealsdisabled" => "Appeals are disabled",
-	"nobanselected" => "No ban selected. Please go back and select a ban.",
-
-	"appealhistory" => "Appeal History",
-	"lastupdatedate" => "Last update date",
-	"lastupdatedby" => "Last update by",
-	"noappealhistory" => "No appeal history",
-
-	//Ban Appeal
-	"makeappeal" => "Make a Appeal",
-	"banappeal" => "Ban Appeal Form",
-	"unbanreason" => "Why should you be unbanned?:",
-	"banappealbtn" => "Submit Ban Appeal",
-
-	//All Bans
-	"noallbans" => "No bans exist",
-
-	//Active bans
-	"noactivebans" => "No active bans",
-
-	//History
-	"nohistorybans" => "No history of bans",
-
-	//Search
-	"searchuser" => "Search for a user",
-	"entersteamid" => "Enter Steam ID...",
-	"results" => "Results",
-
-	//Admin Pages
-	"adminhome" => "Admin home",
-	"appeals" => "Appeals",
-	"banplayer" => "Ban a player",
-	"blockplayer" => "Block a player",
-	"manageservers" => "Manage servers",
-	"manageadmins" => "Manage admins",
-	"settings" => "Settings",
-	"wiki" => "Wiki",
-
-	//Admin Home
-	"welcome" => "Welcome,",
-	"statistics" => "Statistics",
-	"pendingappeals" => "Pending Appeals:",
-	"acceptedappeals" => "Accepted Appeals:",
-	"declinedappeals" => "Declined Appeals:",
-	"adminpermabans" => "Permament Bans:",
-	"admintempbans" => "Temp Bans:",
-	"admintotalbans" => "Total Bans:",
-
-	//Appeals
-	"pendingbanappeals" => "Pending Ban Appeals",
-	"noappeals" => "No open appeals",
-
-	"historyofappeals" => "History of ban appeals",
-	"nohistory" => "No history of appeals",
-	"dateofappeal" => "Date/Time of appeal",
-	"datedecision" => "Date/Time of decision",
-
-	"banappealmessage" => "ban appeal",
-	"banid" => "Ban ID",
-	"vacbans" => "Number of VAC bans",
-	"datebanned" => "Date/Time banned",
-
-	//Ban a player
-	"banlength" => "Ban length (0 for permanent):",
-	"banplayerbtn" => "Ban Player",
-
-	//Block a user
-	"blockmessage" => "Prevent a user from being able to make ban appeals.",
-	"blockuserbtn" => "Block User",
-	"noblockusers" => "No blocked users",
-
-	//Manage Servers
-	"serverid" => "Server ID",
-	"host" => "Host",
-	"addserver" => "Add a new server",
-	"exampleserverid" => "Example: ttt, darkrp, deathrun, bhop, etc",
-	"examplehost" => "Example: 127.0.0.1:27015",
-	"addserverbtn" => "Add Server",
-
-	//Server tooltips
-	"serveridtooltip" => "Enter the servers id e.g: ttt",
-	"hosttooltip" => "Enter the servers IP in the following format: 127.0.0.1:27015",
-
-	//Manage admins
-	"adminsuperadmin" => "Admin/Super Admin",
-	"addnewadmin" => "Add a new admin",
-	"emailoptional" => "Email (Optional)",
-	"addadminbtn" => "Add Admin",
-	"communitytooltip" => "Enter the admins community ID e.g: 76561198030856249",
-
-	//Settings
-	"generalsettings" => "General Settings",
-	"websitename" => "Website name:",
-	"websitelink" => "Website link:",
-	"changelogo" => "Change logo:",
-	"navbackgroundcolor" => "Navigation bar background colour:",
-	"navtextcolour" => "Navigation bar text colour:",
-	"enableappeals" => "Appeals:",
-	"updateinformation" => "Update information",
-
-	"emailsettings" => "Email Settings",
-	"enableemails" => "Enable email support:",
-	"emailtype" => "Email type:",
-	"smtphostname" => "SMTP Hostname:",
-	"smtpexample" => "E.g. smtp@example.com",
-	"port" => "Port:",
-	"portexample" => "E.g. 465",
-	"username" => "Username:",
-	"password" => "Password:",
-	"smtptype" => "SMTP type:",
-	"emailaddress" => "Email address:",
-	"emailfrom" => "Email from:",
-	"updateemailinformation" => "Update Email information",
-	);
+ 
+    //Nav Bar
+    "bans" => "Bans",
+    "home" => "Home",
+    "allbans" => "Alle Bans",
+    "activebans" => "Actieve Bans",
+    "history" => "Geschiedenis",
+    "search" => "Zoeken",
+    "loggedin" => "Aangemeld als:",
+    "admin" => "Beheerder",
+    "help" => "Wiki/Hulp",
+    "logout" => "Uitloggen",
+ 
+    //Footer
+    "copyright" => "Auteursrecht",
+    "created" => "Gemaakt door",
+ 
+    //Common
+    "game" => "Spel",
+    "date" => "Datum/Tijd",
+    "name" => "Naam",
+    "state" => "Staat",
+    "steamid" => "Steam ID",
+    "view" => "Kijk",
+    "reason" => "Reden",
+    "delete" => "Verwijderen",
+    "communityid" => "Community ID",
+    "email" => "E-mail",
+    "type" => "Type",
+    "enabled" => "Ingeschakeld",
+    "disabled" => "Uitgeschakeld",
+    "number" => "#",
+    "accept" => "Accepteren",
+    "decline" => "Weiger",
+    "status" => "Status",
+ 
+    //Error messages
+    "installerror" => "<b>Fout tijdens het laden van de applicatie. Controleer uw config en maak zeker dat je de installatie hebt doorlopen: www.{your-website}.com/bans/admin/install.php</b>",
+    "signin" => "<b>Meld je aan om deze pagina te kunnen zien</b>",
+    "nopermissionerror" => "<b>Je hebt geen toestemming om hier te zijn</b>",
+    "setupcomplete" => "<b>Fout tijdens het laden van de applicatie. De setup is al uitgevoerd!</b>",
+ 
+    //Messages
+    "youmustbeloggedin" => "Je moet ingelogd zijn om een beroep te doen op je ban",
+    "nopermission" => "Je hebt geen toestemming om een beroep te doen op deze ban",
+    "appealmadebold" => "Het beroep is succesvol aangemaakt!",
+    "appealmade" => "Wacht alstublieft op een admin om uw beroep te bekijken.",
+    "appealopen" => "Je hebt al een beroep open, wacht op een admin om hem te bekijken.",
+    "appealblockedbold" => "Je bent verboden om een beroep aan te maken.",
+    "appealblocked" => "Als je denkt dat dit een fout is neem dan contact op met een admin.",
+    "appealacceptedbold" => "Je beroep is geaccepteerd!",
+    "appealaccepted" => "De speler is nu geunbanned.",
+    "appealdeclinedbold" => "Het beroep is geweigert!",
+    "appealdeclined" => "De speler zal verbannen blijven.",
+    "serverdeleted" => "De server is succesvol verwijdert!",
+    "serveradded" => "De server is succesvol toegevoegt!",
+    "installcompletebold" => "De installatie is klaar!",
+    "installcompelte" => "U bent nu vrij om de toepassing te gebruiken.",
+    "playerbanned" => "De speler is verbannen!",
+    "playerblocked" => "De speler is geblokkeerd van het maken van een beroep",
+    "playerunblocked" => "De speler is verwijdert van de blokkeerlijst",
+    "admindeleted" => "Administrator is succesvol verwijdert",
+    "adminadded" => "Administrator is succesvol toegevoegt",
+    "settingsupdatedbold" => "De instellingen zijn geupdate!",
+    "settingsupdated" => "Ga naar home of klik opnieuw op instellingen om de veranderingen te zien",
+    "uploadfailedbold" => "De geüploade afbeelding heeft niet het juiste bestandstype!",
+    "uploadfailed" => "Alleen de volgende bestandstypes zijn toegestaan: .gif .jpg .png",
+ 
+    //Install
+    "welcominstall" => "Welkom bij de Global Ban setup. volg alle instructies aub.",
+    "databasecreated" => "De volgende database is aangemaakt <b>%s</b> en alle vereiste tabellen zijn aangemaakt in uw database.",
+    "enterdetails" => "Vul alle gegevens in voor de standaard admin.",
+    "installcommunityid" => "Steam Community ID: (E.g. 76561198030856249)",
+    "installemailaddress" => "E-mail Adres: (Optioneel. Word gebruikt om e-mails te sturen wanneer er een nieuw beroep is aangemaakt)",
+    "submitadmin" => "Admin opslaan",
+ 
+    //Home
+ 
+    //Servers
+    "os" => "OS",
+    "vac" => "VAC",
+    "hostname" => "Hostname",
+    "players" => "Players",
+    "map" => "Map",
+    "join" => "Join",
+    "noservers" => "Er zijn geen servers",
+    "failedquery" => "Gefaald om de vraag naar de server the versturen, controleer de server!",
+ 
+    //Perma Bans
+    "permabans" => "Laatste permanente bans",
+    "totalpermabans" => "Totaal aantal permanente bans:",
+    "nopermabans" => "Geen permanente bans",
+ 
+    //Temp Bans
+    "tempbans" => "Laatste temp bans",
+    "totaltempbans" => "Totaal aantal temp bans:",
+    "unbanned" => "Geunbant op",
+    "notempbans" => "Geen temp bans",
+ 
+    //Ban Details
+    "bandetails" => "Ban Details",
+    "namebanned" => "Naam wanneer gebanned:",
+    "currentname" => "Huidige naam:",
+    "location" => "Locatie:",
+    "profile" => "Profiel:",
+    "timeofban" => "Tijd van ban:",
+    "server" => "Server:",
+    "bannedby" => "Gebanned door:",
+    "unbantime" => "Unban tijd:",
+    "unbannedby" => "Geunbanned door:",
+    "historyreasonnobans" => "Gebruiker heeft geen vorige bans",
+    "historyreason" => "Gebruiken heeft %s bans gehad",
+    "appealthisban" => "Beroep op deze ban",
+    "appealsdisabled" => "Beroep op een ban is uitgeschakeld",
+    "nobanselected" => "Geen ban geselecteerd. Ga terug and selecteer een ban.",
+ 
+    "appealhistory" => "Beroep Geschiedenis",
+    "lastupdatedate" => "Laatste update datum",
+    "lastupdatedby" => "Laatste update door",
+    "noappealhistory" => "Geen beroep geschiedenis",
+ 
+    //Ban Appeal
+    "makeappeal" => "Maak een beroep",
+    "banappeal" => "Ban beroep Formaat",
+    "unbanreason" => "Waarom zou u geunbanned moeten worden?:",
+    "banappealbtn" => "Geef u ban beroep op",
+ 
+    //All Bans
+    "noallbans" => "Er bestaan geen bans",
+ 
+    //Active bans
+    "noactivebans" => "Geen actieve bans",
+ 
+    //History
+    "nohistorybans" => "Geen geschiedenis van bans",
+ 
+    //Search
+    "searchuser" => "Zoek voor een gebruiker",
+    "entersteamid" => "Voer uw Steam ID in...",
+    "results" => "Resultaten",
+ 
+    //Admin Pages
+    "adminhome" => "Administrator home",
+    "appeals" => "Beroepen",
+    "banplayer" => "Ban een speler",
+    "blockplayer" => "Blokkeer een speler",
+    "manageservers" => "Beheer servers",
+    "manageadmins" => "Beheer admins",
+    "settings" => "Instellingen",
+    "wiki" => "Wiki",
+ 
+    //Admin Home
+    "welcome" => "Welkom,",
+    "statistics" => "Statistieken",
+    "pendingappeals" => "Onbesliste beroepen:",
+    "acceptedappeals" => "Geaccepteerde beroepen:",
+    "declinedappeals" => "Geweigerde beroepen:",
+    "adminpermabans" => "Permamente Bans:",
+    "admintempbans" => "Tijdelijke Bans:",
+    "admintotalbans" => "Totaal aantal Bans:",
+ 
+    //Appeals
+    "pendingbanappeals" => "Onbesliste beroepen",
+    "noappeals" => "Geen open beroepen",
+ 
+    "historyofappeals" => "Geschiedenis van beroepen",
+    "nohistory" => "Geen geschiedenis van beroepen",
+    "dateofappeal" => "Datum/Tijd van het beroep",
+    "datedecision" => "Datum/Tijd van de beslissing",
+ 
+    "banappealmessage" => "Ban beroep",
+    "banid" => "Ban ID",
+    "vacbans" => "Aantal VAC bans",
+    "datebanned" => "Datum/Tijd gebanned",
+ 
+    //Ban a player
+    "banlength" => "Ban lente (0 voor permanent):",
+    "banplayerbtn" => "Ban speler",
+ 
+    //Block a user
+    "blockmessage" => "Voorkom een gebruiker van het maken van beroepen",
+    "blockuserbtn" => "Blokkeer speler",
+    "noblockusers" => "Geen geblokkeerde spelers",
+ 
+    //Manage Servers
+    "serverid" => "Server ID",
+    "host" => "Host",
+    "addserver" => "Voeg een nieuwe server toe",
+    "exampleserverid" => "Voorbeeld: ttt, darkrp, deathrun, bhop, etc",
+    "examplehost" => "Voorbeeld: 127.0.0.1:27015",
+    "addserverbtn" => "Server toevoegen",
+ 
+    //Server tooltips
+    "serveridtooltip" => "Voer de servers id in voorbeeld: ttt",
+    "hosttooltip" => "Voer de server ip in het volgende formaat in: 127.0.0.1:27015",
+ 
+    //Manage admins
+    "adminsuperadmin" => "Admin/Super Admin",
+    "addnewadmin" => "Voeg een nieuwe admin toe",
+    "emailoptional" => "E-mail (Niet verplicht)",
+    "addadminbtn" => "Administrator toevoegen",
+    "communitytooltip" => "Voer de administrators community ID in voorbeeld: 76561198030856249",
+ 
+    //Settings
+    "generalsettings" => "Algemene instellingen",
+    "websitename" => "Website naam:",
+    "websitelink" => "Website link:",
+    "changelogo" => "Verander logo:",
+    "navbackgroundcolor" => "Navigatie bar achtergrond kleur:",
+    "navtextcolour" => "Navigatie bar text kleur:",
+    "enableappeals" => "Beroepen:",
+    "updateinformation" => "Update informatie",
+ 
+    "emailsettings" => "E-mail Instellingen",
+    "enableemails" => "Zet e-mail support aan:",
+    "emailtype" => "E-mail type:",
+    "smtphostname" => "SMTP Hostname:",
+    "smtpexample" => "Voorbeeld: smtp@example.com",
+    "port" => "Port:",
+    "portexample" => "Voorbeeld: 465",
+    "username" => "Gebruikersnaam:",
+    "password" => "Wachtwoord:",
+    "smtptype" => "SMTP type:",
+    "emailaddress" => "E-mail adres:",
+    "emailfrom" => "Email van:",
+    "updateemailinformation" => "Update E-mail informatie",
+    );
 ?>
