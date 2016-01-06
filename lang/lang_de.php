1079675d49661dfd5fa146bf40ef89aebe3b1e3f
Jwoolley <jwoolley44@hotmail.co.uk>
2016-01-06 00:47:01 +0000

    Added all required messages

--- lang/lang_de.php
+++ lang/lang_de.php
@@ -10,7 +10,7 @@ $lang = array(
 	"allbans" => "All Bans",
 	"activebans" => "Active Bans",
 	"history" => "History",
-	"search" => "Seach",
+	"search" => "Search",
 	"loggedin" => "Logged in as:",
 	"admin" => "Admin",
 	"help" => "Wiki/Help",
@@ -37,20 +37,47 @@ $lang = array(
 	"number" => "#",
 	"accept" => "Accept",
 	"decline" => "Decline",
+	"status" => "Status",
+
+	//Error messages
+	"installerror" => "<b>Error whilst loading Application. Please check your config and make sure you have ran the install: www.{your-website}.com/bans/admin/install.php</b>",
+	"signin" => "<b>Please sign in to see this page</b>",
+	"nopermissionerror" => "<b>You do not have permission to be here</b>",
+	"setupcomplete" => "<b>Error whilst loading Application. The setup has already been performed!</b>",
 
 	//Messages
 	"youmustbeloggedin" => "You must be logged in to appeal a ban",
 	"nopermission" => "You do not have permission to appeal this ban",
 	"appealmadebold" => "The appeal has been successfully made!",
 	"appealmade" => "Please wait for an admin to review the appeal.",
+	"appealopen" => "You already have a appeal open please wait for a admin to review it!",
+	"appealblockedbold" => "It seems you have been banned from making appeals",
+	"appealblocked" => "If you believe this is a mistake please contact a admin",
 	"appealacceptedbold" => "The appeal has been accepted!",
 	"appealaccepted" => "The player has now been unbanned.",
 	"appealdeclinedbold" => "The appeal has been declined!",
 	"appealdeclined" => "The player will remain banned.",
-	"serverdelete" => "Server has been successfully deleted!",
+	"serverdeleted" => "Server has been successfully deleted!",
 	"serveradded" => "Server has been successfully been added!",
-	"installcompletebold" => "The installtion is now complete!",
+	"installcompletebold" => "The installation is now complete!",
 	"installcompelte" => "You are now free to use the application.",
+	"playerbanned" => "Player has been banned",
+	"playerblocked" => "Player has been blocked from making appeals",
+	"playerunblocked" => "Player has been removed from the blocked list",
+	"admindeleted" => "Admin has been successfully deleted",
+	"adminadded" => "Admin has been successfully been added",
+	"settingsupdatedbold" => "The settings have been updated!",
+	"settingsupdated" => "To pick up the new changes you need to go to the home page or click settings again",
+	"uploadfailedbold" => "The uploaded image did not have the right file type!",
+	"uploadfailed" => "Only the following types are allowed: .gif .jpg .png",
+
+	//Install
+	"welcominstall" => "Welcome to Global Ban setup. Please follow all instructions.",
+	"databasecreated" => "The following database has been created <b>%s</b> and all required tables have been made in your database.",
+	"enterdetails" => "Please enter all the details in for the default admin.",
+	"installcommunityid" => "Steam Community ID: (E.g. 76561198030856249)",
+	"installemailaddress" => "Email Address: (Optional. Used to send emails when a new appeal has been submitted)",
+	"submitadmin" => "Submit Admin",
 
 	//Home
 
@@ -58,10 +85,11 @@ $lang = array(
 	"os" => "OS",
 	"vac" => "VAC",
 	"hostname" => "Hostname",
-	"players" => "Players"
+	"players" => "Players",
 	"map" => "Map",
 	"join" => "Join",
 	"noservers" => "No servers exist",
+	"failedquery" => "Failed to query the server. Please check the server!",
 
 	//Perma Bans
 	"permabans" => "Latest permanent bans",
@@ -85,15 +113,19 @@ $lang = array(
 	"bannedby" => "Banned by:",
 	"unbantime" => "Unban time:",
 	"unbannedby" => "Unbanned by:",
+	"historyreasonnobans" => "User has no previous bans",
 	"historyreason" => "User has had %s previous ban(s)",
 	"appealthisban" => "Appeal this ban",
+	"appealsdisabled" => "Appeals are disabled",
 	"nobanselected" => "No ban selected. Please go back and select a ban.",
 
 	"appealhistory" => "Appeal History",
 	"lastupdatedate" => "Last update date",
 	"lastupdatedby" => "Last update by",
+	"noappealhistory" => "No appeal history",
 
 	//Ban Appeal
+	"makeappeal" => "Make a Appeal",
 	"banappeal" => "Ban Appeal Form",
 	"unbanreason" => "Why should you be unbanned?:",
 	"banappealbtn" => "Submit Ban Appeal",
@@ -110,15 +142,15 @@ $lang = array(
 	//Search
 	"searchuser" => "Search for a user",
 	"entersteamid" => "Enter Steam ID...",
-	"search" => "Search",
+	"results" => "Results",
 
 	//Admin Pages
-	"adminhome" => "Admin Home",
+	"adminhome" => "Admin home",
 	"appeals" => "Appeals",
 	"banplayer" => "Ban a player",
 	"blockplayer" => "Block a player",
-	"manageservers" => "Manage Servers",
-	"manageadmins" => "Manage Admins",
+	"manageservers" => "Manage servers",
+	"manageadmins" => "Manage admins",
 	"settings" => "Settings",
 	"wiki" => "Wiki",
 
@@ -134,24 +166,26 @@ $lang = array(
 
 	//Appeals
 	"pendingbanappeals" => "Pending Ban Appeals",
-	"status" => "Status",
-	"datedecision" => "Date/Time of decision",
+	"noappeals" => "No open appeals",
 
 	"historyofappeals" => "History of ban appeals",
-	"dateofappeal" = "Date/Time of appeal",
+	"nohistory" => "No history of appeals",
+	"dateofappeal" => "Date/Time of appeal",
+	"datedecision" => "Date/Time of decision",
 
 	"banappealmessage" => "ban appeal",
 	"banid" => "Ban ID",
 	"vacbans" => "Number of VAC bans",
+	"datebanned" => "Date/Time banned",
 
 	//Ban a player
-	"banlength" => "Ban Length (0 for permanent):",
+	"banlength" => "Ban length (0 for permanent):",
 	"banplayerbtn" => "Ban Player",
 
 	//Block a user
 	"blockmessage" => "Prevent a user from being able to make ban appeals.",
 	"blockuserbtn" => "Block User",
-	"noblockusers" => "No Blocked Users",
+	"noblockusers" => "No blocked users",
 
 	//Manage Servers
 	"serverid" => "Server ID",
@@ -161,10 +195,16 @@ $lang = array(
 	"examplehost" => "Example: 127.0.0.1:27015",
 	"addserverbtn" => "Add Server",
 
+	//Server tooltips
+	"serveridtooltip" => "Enter the servers id e.g: ttt",
+	"hosttooltip" => "Enter the servers IP in the following format: 127.0.0.1:27015",
+
 	//Manage admins
 	"adminsuperadmin" => "Admin/Super Admin",
 	"addnewadmin" => "Add a new admin",
 	"emailoptional" => "Email (Optional)",
+	"addadminbtn" => "Add Admin",
+	"communitytooltip" => "Enter the admins community ID e.g: 76561198030856249",
 
 	//Settings
 	"generalsettings" => "General Settings",
@@ -173,6 +213,7 @@ $lang = array(
 	"changelogo" => "Change logo:",
 	"navbackgroundcolor" => "Navigation bar background colour:",
 	"navtextcolour" => "Navigation bar text colour:",
+	"enableappeals" => "Appeals:",
 	"updateinformation" => "Update information",
 
 	"emailsettings" => "Email Settings",
@@ -184,8 +225,9 @@ $lang = array(
 	"portexample" => "E.g. 465",
 	"username" => "Username:",
 	"password" => "Password:",
-	"smtptype" => "SMTP type",
-	"emailaddrees" => "Email addrees:",
+	"smtptype" => "SMTP type:",
+	"emailaddress" => "Email address:",
 	"emailfrom" => "Email from:",
+	"updateemailinformation" => "Update Email information",
 	);
 ?>
